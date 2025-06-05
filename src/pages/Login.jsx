0641772dddeac4a6074c50669ed66bfce9323bb8
AdnanSameer1724 <adnansameer1724@gmail.com>
2025-06-05 18:00:23 +0530

    Initial commit just installed dependencies

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <button className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700">
+        Login
+      </button>
+    </div>
+  );
+}
